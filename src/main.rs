1c6582c22b7058fcad2c820d8f641d2834d9ad75
nauxscript <nauxscript@gmail.com>
2024-08-19 22:54:11 +0800

    feat: rcli v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    // println!("Hello, world!");
+    let opts = Opts::parse();
+    // println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
---
+++ src/opts.rs
@@ -0,0 +1,35 @@
+use clap::Parser;
+use std::path::Path;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "show CSV or convert CSV to other format")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(file_name: &str) -> Result<String, String> {
+    if Path::new(file_name).exists() {
+        Ok(file_name.into())
+    } else {
+        Err(format!("File not found: {}", file_name))
+    }
+}
