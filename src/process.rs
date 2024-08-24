b3eb35de695f75237defed17b5e9b1d2ff386afb
nauxscript <nauxscript@gmail.com>
2024-08-24 12:04:01 +0800

    refactor: dynamic header csv

--- src/process.rs
+++ src/process.rs
@@ -1,26 +1,16 @@
 use csv::Reader;
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
 
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let mut ret: Vec<Player> = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record)
-        // println!("{:?}", record);
+    let mut ret = Vec::with_capacity(128);
+    let header = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = header.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
+        // println!("{:?}", ret);
     }
     let json = serde_json::to_string_pretty(&ret)?;
     fs::write(output, json)?;
