2b4843c67e0cd6a67750f4136e574c7a8b936d41
nauxscript <nauxscript@gmail.com>
2024-08-28 09:27:13 +0800

    feat: generate random password

--- src/opts.rs
+++ src/opts.rs
@@ -18,6 +18,22 @@ pub enum OutputFormat {
 pub enum SubCommand {
     #[command(name = "csv", about = "show CSV or convert CSV to other format")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "generate a random password")]
+    GenPass(GenPassOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
 }
 
 #[derive(Debug, Parser)]
