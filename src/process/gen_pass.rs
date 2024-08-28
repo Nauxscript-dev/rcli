9e28799773b69acd325d7a72577a2733ace02690
nauxscript <nauxscript@gmail.com>
2024-08-28 09:39:18 +0800

    feat: support to estimate the strength of password

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPERCASE: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LOWERCASE: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
@@ -43,7 +44,14 @@ pub fn process_genpass(
         let c = chars.choose(&mut rng).expect("chars won't be empty");
         password.push(*c);
     }
+
     password.shuffle(&mut rng);
-    println!("{}", String::from_utf8(password)?);
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    // output password strength in stderr
+    let estimate = zxcvbn(&password, &[]);
+    eprintln!("Password strength: {}", estimate.score());
+
     Ok(())
 }
